41d44121517e0b815d1c1e27977020d440929620
xChuCx <xChuCx@gmail.com>
2020-01-27 22:55:52 +0300

    true baseline

--- services/api/RecSystem.py
+++ services/api/RecSystem.py
@@ -3,9 +3,15 @@
 from flask import Blueprint, request
 from flask_restful import Resource, Api
 from flask import jsonify
+from services.utils.Predictor import Predictor
 
 RecSystem_blueprint = Blueprint('RecSystem', __name__)
 api = Api(RecSystem_blueprint)
+
+ctl = Predictor(
+    product_csv_path="../assets/products.csv",
+    model_pickled_path="../assets/model.pkl"
+)
 
 
 class Status(Resource):
@@ -17,9 +23,10 @@ class Status(Resource):
 
 class Recommend(Resource):
     def post(self):
-        post_data = request.get_json()
+        r = request.json
+        recs = ctl.predict(r.get("transaction_history", []))[:30]
         response_object = jsonify({
-            'recommended_products': 'loh'
+            'recommended_products': recs
         })
         response_object.status_code = 200
         return response_object
